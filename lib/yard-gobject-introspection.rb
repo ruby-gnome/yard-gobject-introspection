cdcb7c4188c0e472bb33ae2351bb40ee76595818
cedlemo <cedlemo@gmx.com>
2016-12-02 14:42:21 +0100

    First basic loader

--- lib/yard-gobject-introspection.rb
+++ lib/yard-gobject-introspection.rb
@@ -1,9 +1,23 @@
 require "yard"
+require "gobject-introspection"
 
-class MyModuleHandler < YARD::Handlers::Ruby::Base
+class GObjectIntropsectionHandler < YARD::Handlers::Ruby::Base
   handles :module
 
   def process
-    puts "Handling a module named #{statement[0].source}"
+    base = File.join(File.dirname(File.expand_path(statement.file)))
+    $LOAD_PATH.unshift(base)
+
+    puts "-- Load module info"
+
+    require File.expand_path(statement.file)
+    module_name = statement[0].source
+    puts module_name
+    current_module = Object.const_get("#{module_name}")
+    current_module.init if current_module.respond_to?(:init)
+
+    current_module.constants.each do |c|
+      puts "#{c} #{current_module.const_get(c).class}"
+    end
   end
 end

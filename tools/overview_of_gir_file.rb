5303601c257d735b9081d562b05fa99119e64b66
cedlemo <cedlemo@gmx.com>
2017-02-18 11:43:56 +0100

    Support two more arguments in overview_of_gir_file.rb

--- tools/overview_of_gir_file.rb
+++ tools/overview_of_gir_file.rb
@@ -7,16 +7,25 @@ gir_path = "/usr/share/gir-1.0"
 def print_help
 puts %q(
 usage:
-  overview_of_gir_file.rb pattern
+  overview_of_gir_file.rb pattern [type] [expand]
 
   pattern is for example Atk for the file Atk-1.0.gir
 
 )
 end
 
-if ARGV.size != 1
+if ARGV.size < 1 && ARGV.size > 3
+  STDERR.puts "Bad number of arguments"
   print_help
   exit 1
+end
+
+expand = false
+type = nil
+expand = ARGV.include?("expand")
+
+ARGV[1..2].each do |arg|
+  type = arg unless arg == "expand"
 end
 
 file_name = ARGV[0]
@@ -44,9 +53,19 @@ gir_document.elements.each("repository/namespace/*") do |element|
   end
 end
 
-element_names.each do |name, elements|
+def display_information(name, elements, expand)
   puts name
-  elements.each do |e|
-    puts "\t* #{e}"
+  if expand
+    elements.each do |e|
+      puts "\t* #{e}"
+    end
+  end
+end
+
+element_names.each do |name, elements|
+  if type
+    display_information(name, elements, expand) if name == type
+  else
+    display_information(name, elements, expand)
   end
 end

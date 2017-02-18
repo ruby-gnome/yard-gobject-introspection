af897ed5182eaab60cb2c12c52ae0b4836e26cf8
cedlemo <cedlemo@gmx.com>
2017-02-18 11:11:57 +0100

    Add tool overview_of_gir.file

---
+++ tools/overview_of_gir_file.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+
+require "rexml/document"
+
+gir_path = "/usr/share/gir-1.0"
+
+def print_help
+puts %q(
+usage:
+  overview_of_gir_file.rb pattern
+
+  pattern is for example Atk for the file Atk-1.0.gir
+
+)
+end
+
+if ARGV.size != 1
+  print_help
+  exit 1
+end
+
+file_name = ARGV[0]
+
+girs_files = Dir.glob("#{gir_path}/#{file_name}-?.*gir")
+gir_file = girs_files.last
+
+if gir_file.nil?
+  STDERR.puts "#{ARGV[0]} does not match any gir files"
+  print_help
+  exit 1
+end
+
+file = File.new(gir_file)
+gir_document = REXML::Document.new file
+
+element_names = {}
+gir_document.elements.each("repository/namespace/*") do |element|
+  attr_name = element.attributes["name"]
+
+  if element_names[element.name].class == Array
+    element_names[element.name] << attr_name
+  else
+    element_names[element.name] = [attr_name]
+  end
+end
+
+element_names.each do |name, elements|
+  puts name
+  elements.each do |e|
+    puts "\t* #{e}"
+  end
+end
